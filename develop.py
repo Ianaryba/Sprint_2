7b181af039ee1621ffd790d0b420912843baa869
Калинина Яна <pepper063@mail.ru>
2024-07-27 15:51:21 +0400

    chernovik

--- develop.py
+++ develop.py
@@ -28,41 +28,41 @@ class TestBooksCollector:
         self.assertEqual(self.collector.get_book_genre("The Shining"), "Ужасы")
         from main import BooksCollector
 
-        class TestBooksCollector:
-            def setUp(self):
-                self.collector = BooksCollector()
+class TestBooksCollector:
+    def setUp(self):
+        self.collector = BooksCollector()
 
-            def test_add_new_book(self):
-                self.collector.add_new_book("The Shining")
-                assert "The Shining" in self.collector.books_genre
+    def test_add_new_book(self):
+        self.collector.add_new_book("The Shining")
+        assert "The Shining" in self.collector.books_genre
 
-            def test_set_book_genre(self):
-                self.collector.add_new_book("The Shining")
-                self.collector.set_book_genre("The Shining", "Horror")
-                assert self.collector.get_book_genre("The Shining") == "Horror"
+    def test_set_book_genre(self):
+        self.collector.add_new_book("The Shining")
+        self.collector.set_book_genre("The Shining", "Horror")
+        assert self.collector.get_book_genre("The Shining") == "Horror"
 
-            def test_get_books_with_specific_genre(self):
-                self.collector.add_new_book("The Shining")
-                self.collector.set_book_genre("The Shining", "Horror")
-                self.collector.add_new_book("The Godfather")
-                self.collector.set_book_genre("The Godfather", "Crime")
-                assert "The Shining" in self.collector.get_books_with_specific_genre("Horror")
-                assert "The Godfather" in self.collector.get_books_with_specific_genre("Crime")
+    def test_get_books_with_specific_genre(self):
+        self.collector.add_new_book("The Shining")
+        self.collector.set_book_genre("The Shining", "Horror")
+        self.collector.add_new_book("The Godfather")
+        self.collector.set_book_genre("The Godfather", "Crime")
+        assert "The Shining" in self.collector.get_books_with_specific_genre("Horror")
+        assert "The Godfather" in self.collector.get_books_with_specific_genre("Crime")
 
-            def test_get_books_for_children(self):
-                self.collector.add_new_book("The Shining")
-                self.collector.set_book_genre("The Shining", "Horror")
-                self.collector.add_new_book("The Godfather")
-                self.collector.set_book_genre("The Godfather", "Crime")
-                self.collector.add_new_book("Toy Story")
-                self.collector.set_book_genre("Toy Story", "Cartoons")
-                assert "Toy Story" in self.collector.get_books_for_children()
-                assert "The Shining" not in self.collector.get_books_for_children()
-                assert "The Godfather" not in self.collector.get_books_for_children()
+    def test_get_books_for_children(self):
+        self.collector.add_new_book("The Shining")
+        self.collector.set_book_genre("The Shining", "Horror")
+        self.collector.add_new_book("The Godfather")
+        self.collector.set_book_genre("The Godfather", "Crime")
+        self.collector.add_new_book("Toy Story")
+        self.collector.set_book_genre("Toy Story", "Cartoons")
+        assert "Toy Story" in self.collector.get_books_for_children()
+        assert "The Shining" not in self.collector.get_books_for_children()
+        assert "The Godfather" not in self.collector.get_books_for_children()
 
-            def test_add_and_delete_book_from_favorites(self):
-                self.collector.add_new_book("The Shining")
-                self.collector.add_book_in_favorites("The Shining")
-                assert "The Shining" in self.collector.get_list_of_favorites_books()
-                self.collector.delete_book_from_favorites("The Shining")
-                assert "The Shining" not in self.collector.get_list_of_favorites_books()
+    def test_add_and_delete_book_from_favorites(self):
+        self.collector.add_new_book("The Shining")
+        self.collector.add_book_in_favorites("The Shining")
+        assert "The Shining" in self.collector.get_list_of_favorites_books()
+        self.collector.delete_book_from_favorites("The Shining")
+        assert "The Shining" not in self.collector.get_list_of_favorites_books()
